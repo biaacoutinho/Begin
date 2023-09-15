8374f2f6e7127fd318f3f955055b0b3926cdcfc9
Beatriz Coutinho <beatrizjcoutinho@gmail.com>
2023-09-14 23:26:50 -0300

    add the controllers of the table "Voluntario"

---
+++ backend/src/Controller/voluntarioController.js
@@ -0,0 +1,98 @@
+const db = require('../db')
+
+exports.getVoluntarios = ('/voluntarios', async(req, res) => {
+  try {
+    const { data, error } = await db.from('Voluntario').select('*');
+    if (error) {
+      throw error;
+    }
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+exports.getVoluntario = ('/voluntario/:username', async(req, res) => {
+  const username = req.params.username;
+
+  try {
+    const { data, error } = await db.from('Voluntario').select('*').eq('username', username);
+    if (error) {
+      throw error;
+    }
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+})
+
+exports.putVoluntario = ('/voluntario', async(req, res) => {
+  const username = req.body.username;
+  const nome = req.body.nome;
+  const senha = req.body.senha;
+  const idioma = req.body.idioma;
+  const cpf = req.body.cpf;
+  const telefone = req.body.telefone;
+  const habilidade = req.body.habilidade;
+  const email = req.body.email;
+
+  try {
+    const { data, error } = await db
+      .from('Voluntario')
+      .insert([
+        { username: username, nome: nome, senha: senha, idioma: idioma, cpf: cpf, telefone: telefone, habilidade: habilidade, email: email },
+      ])
+      .select()
+
+    if (error) {
+    throw error;
+    }
+    res.json(data);
+  } catch (error) {
+      res.status(500).json({ error: error.message });
+  }
+});
+
+exports.postVoluntario  = ('/voluntario/:username', async(req, res) => {
+  const username = req.params.username;
+  const nome = req.body.nome;
+  const senha = req.body.senha;
+  const idioma = req.body.idioma;
+  const pais = req.body.paisDeOrigem;
+  const telefone = req.body.telefone;
+  const email = req.body.email;
+
+  try {
+    const { data, error } = await db
+      .from('Voluntario')
+      .update({ nome: nome, senha: senha, idioma: idioma, paisDeOrigem: pais, telefone: telefone, email: email })
+      .eq('username', username)
+      .select()
+
+    if (error) {
+    throw error;
+    }
+    res.json(data);
+  } catch (error) {
+      res.status(500).json({ error: error.message });
+  }
+});
+
+exports.deleteVoluntario = ('/voluntario/:username', async(req, res) => {
+  const username = req.params.username; 
+
+  try {
+    const { data, error } = await db
+      .from('Voluntario')
+      .delete()
+      .eq('username', username)
+      .select()
+
+    if (error) {
+    throw error;
+    }
+    res.json(data);
+  } catch (error) {
+      res.status(500).json({ error: error.message });
+  }
+})
--- backend/src/router.js
+++ backend/src/router.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const ctrRef = require('./Controller/refugiadoController')
+const ctrVolun = require('./Controller/voluntarioController')
 const router = express.Router()
 
 router.get('/refugiados', ctrRef.getRefugiados)
@@ -7,5 +8,11 @@ router.get('/refugiado/:username', ctrRef.getRefugiado)
 router.put('/refugiado', ctrRef.putRefugiado)
 router.post('/refugiado/:username', ctrRef.postRefugiado)
 router.delete('/refugiado/:username', ctrRef.deleteRefugiado)
+
+router.get('/voluntarios', ctrVolun.getVoluntarios)
+router.get('/voluntario/:username', ctrVolun.getVoluntario)
+router.put('/voluntario', ctrVolun.putVoluntario)
+router.post('/voluntario/:username', ctrVolun.postVoluntario)
+router.delete('/voluntario/:username', ctrVolun.deleteVoluntario)
 
 module.exports = router
