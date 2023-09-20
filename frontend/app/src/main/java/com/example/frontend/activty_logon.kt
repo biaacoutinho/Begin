84b1692f585ac2d7a96c10776c460963a6314ee1
EDUARDA GRAZIELE DE PAIVA <u22125@academico.cotuca.unicamp.br>
2023-09-20 10:48:46 -0300

    Tela cadastro voluntário

---
+++ frontend/app/src/main/java/com/example/frontend/activty_logon.kt
@@ -0,0 +1,11 @@
+package com.example.frontend
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activty_logon : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_activty_logon)
+    }
+}
