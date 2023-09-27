5e925a133cbb794200da139e5de110afd05c70b9
Beatriz Coutinho <beatrizjcoutinho@gmail.com>
2023-09-26 23:09:03 -0300

    add the intent of login to logon page

--- frontend/app/src/main/java/com/example/frontend/Login.kt
+++ frontend/app/src/main/java/com/example/frontend/Login.kt
@@ -1,11 +1,20 @@
 package com.example.frontend
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val tvCadastrar = findViewById<TextView>(R.id.tvCadastrar)
+
+        tvCadastrar.setOnClickListener({
+            val intent = Intent(this, activty_logon::class.java)
+            startActivity(intent)
+        })
     }
 }
