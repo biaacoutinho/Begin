a8b8423b94e8c1ba8001771122b915f8aefe719d
Beatriz Coutinho <beatrizjcoutinho@gmail.com>
2023-10-10 15:08:55 -0300

    add info on refugiado's profile

--- frontend/app/src/main/java/com/example/frontend/perfilRefugiado.kt
+++ frontend/app/src/main/java/com/example/frontend/perfilRefugiado.kt
@@ -1,11 +1,38 @@
 package com.example.frontend
 
+import android.app.Application
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class perfilRefugiado : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_perfil_refugiado)
+
+        val tvNome = findViewById<TextView>(R.id.tvNome)
+        val tvUsername = findViewById<TextView>(R.id.tvUsername)
+        val tvIdiomas = findViewById<TextView>(R.id.tvIdiomas)
+        val tvPais = findViewById<TextView>(R.id.tvPais)
+        val tvTelefone = findViewById<TextView>(R.id.tvTelefone)
+        val tvEmail = findViewById<TextView>(R.id.tvEmail)
+
+        val gUser = application as GlobalUser
+        val user = gUser.getGlobalRefugiado()
+
+        tvNome.text = user?.nome
+        tvUsername.text = "@" + user?.username
+        tvIdiomas.text = user?.idioma
+        tvPais.text = user?.paisOrigem
+
+        if (user?.telefone != null)
+            tvTelefone.text = user?.telefone
+        else
+            tvTelefone.text = "Nenhum telefone foi cadastrado"
+
+        if (user?.email != null)
+            tvEmail.text = user?.email
+        else
+            tvEmail.text = "Nenhum email foi cadastrado"
     }
 }
