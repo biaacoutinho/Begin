3ba1d11db005dda6a0783a28ea0906a7b1bd231c
Beatriz Coutinho <beatrizjcoutinho@gmail.com>
2023-10-10 01:55:40 -0300

    getGlobalRefugiado is working

--- frontend/app/src/main/java/com/example/frontend/GlobalUser.kt
+++ frontend/app/src/main/java/com/example/frontend/GlobalUser.kt
@@ -8,15 +8,21 @@ class GlobalUser: Application() {
     public lateinit var voluntario : Voluntario;
     public lateinit var refugiado : Refugiado;
 
-    fun getGlobalVoluntario(): Voluntario {
-        return voluntario
+    fun getGlobalVoluntario(): Voluntario? {
+        if (this::voluntario.isInitialized)
+            return voluntario
+        else
+            return null
     }
     fun setGlobalVoluntario(volun: Voluntario) {
         voluntario = volun
     }
 
-    fun getGlobalRefugiado(): Refugiado {
-        return refugiado
+    fun getGlobalRefugiado(): Refugiado? {
+        if (this::refugiado.isInitialized)
+            return refugiado
+        else
+            return null
     }
     fun setGlobalRefugiado(ref: Refugiado) {
         refugiado = ref
--- frontend/app/src/main/java/com/example/frontend/InicialRefugiado.kt
+++ frontend/app/src/main/java/com/example/frontend/InicialRefugiado.kt
@@ -5,18 +5,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class InicialRefugiado : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicial_refugiado)
 
-        val btnPerfilRefugiado = findViewById<Button>(R.id.btnPerfilRefugiado)
+        val btnPerfilRefugiado = findViewById<FloatingActionButton>(R.id.btnPerfilRefugiado)
 
         btnPerfilRefugiado.setOnClickListener(){
             val gUser = application as GlobalUser
             val user = gUser.getGlobalRefugiado()
-            Log.d("usuario", user.nome)
+
             if (user != null)
                 startActivity(Intent(this, perfilRefugiado::class.java))
             else
